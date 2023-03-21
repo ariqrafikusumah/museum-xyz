abfadec9a4e950571905c39ec810b6ec20784d83
ariqrafikusumah <ariqrafikusumah@gmail.com>
2023-03-21 13:57:39 +0700

    init 2

--- src/pages/admin/Knn_form.jsx
+++ src/pages/admin/Knn_form.jsx
@@ -6,35 +6,35 @@ function KnnForm() {
     const [input, setInput] = useState('');
 
     // fungsi untuk menghitung label kelas dengan metode k-NN
-    // function calculateKnn() {
-    //     const data = dataset
-    //         .split('\n')
-    //         .map((line) => line.trim().split(','))
-    //         .filter((line) => line.length === 3);
-    //     const inputValues = input.trim().split(',').map((value) => parseFloat(value));
-    //     if (data.length === 0 || inputValues.length !== 2) {
-    //         return; // keluar dari fungsi jika dataset atau input tidak valid
-    //     }
-    //     const distances = data.map((row) => {
-    //         const dx = row[0] - inputValues[0];
-    //         const dy = row[1] - inputValues[1];
-    //         return Math.sqrt(dx * dx + dy * dy);
-    //     });
-    //     const sortedIndices = distances
-    //         .map((distance, index) => ({ distance, index }))
-    //         .sort((a, b) => a.distance - b.distance)
-    //         .map((obj) => obj.index);
-    //     const kNearestClasses = sortedIndices.slice(0, k).map((index) => data[index][2]);
-    //     const classCounts = kNearestClasses.reduce((acc, cur) => {
-    //         acc[cur] = (acc[cur] || 0) + 1;
-    //         return acc;
-    //     }, {});
-    //     const predictedClass = Object.entries(classCounts).reduce(
-    //         (max, [key, value]) => (value > max[1] ? [key, value] : max),
-    //         ['', 0]
-    //     )[0];
-    //     console.log('Predicted class:', predictedClass);
-    // }
+    function calculateKnn() {
+        // const data = dataset
+        //     .split('\n')
+        //     .map((line) => line.trim().split(','))
+        //     .filter((line) => line.length === 3);
+        // const inputValues = input.trim().split(',').map((value) => parseFloat(value));
+        // if (data.length === 0 || inputValues.length !== 2) {
+        //     return; // keluar dari fungsi jika dataset atau input tidak valid
+        // }
+        // const distances = data.map((row) => {
+        //     const dx = row[0] - inputValues[0];
+        //     const dy = row[1] - inputValues[1];
+        //     return Math.sqrt(dx * dx + dy * dy);
+        // });
+        // const sortedIndices = distances
+        //     .map((distance, index) => ({ distance, index }))
+        //     .sort((a, b) => a.distance - b.distance)
+        //     .map((obj) => obj.index);
+        // const kNearestClasses = sortedIndices.slice(0, k).map((index) => data[index][2]);
+        // const classCounts = kNearestClasses.reduce((acc, cur) => {
+        //     acc[cur] = (acc[cur] || 0) + 1;
+        //     return acc;
+        // }, {});
+        // const predictedClass = Object.entries(classCounts).reduce(
+        //     (max, [key, value]) => (value > max[1] ? [key, value] : max),
+        //     ['', 0]
+        // )[0];
+        // console.log('Predicted class:', predictedClass);
+    }
 
     return (
         <div>
